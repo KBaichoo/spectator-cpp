39d681e45e84e1237333527d1a753efbf639f9ab
Kevin Baichoo <kbaichoo@netflix.com>
2025-09-05 10:48:54 -0700

    Get the test to run.
    Signed-off-by: Kevin Baichoo <kbaichoo@netflix.com>

--- spectator/test_main.cc
+++ spectator/test_main.cc
@@ -1,8 +1,8 @@
-#include "backward.hpp"
+// #include "backward.hpp"
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  backward::SignalHandling sh;
+  // backward::SignalHandling sh;
   return RUN_ALL_TESTS();
 }
